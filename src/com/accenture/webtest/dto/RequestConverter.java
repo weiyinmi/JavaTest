175d44a7ac155de59044f0e207be205f317bfa84
Yin Wei <yinwqz@126.com>
2017-09-28 20:04:32 +0800

    commit converter and reflection

---
+++ src/com/accenture/webtest/dto/RequestConverter.java
@@ -0,0 +1,45 @@
+package com.accenture.webtest.dto;
+
+//import net.sf.json.JSONObject;
+import org.json.JSONObject;
+
+public class RequestConverter implements Converter<JosephCircleRequest> {
+
+	@Override
+	public JosephCircleRequest fromJson(JSONObject jsonObject) {
+
+		/*
+		 * JosephCircleRequest request = new JosephCircleRequest(); JosephObject
+		 * josephObject =new JosephObject();
+		 * 
+		 * JSONObject jsonObj = jsonObject.getJSONObject("circle");
+		 * 
+		 * josephObject.setStart(jsonObj.getString("start"));
+		 * josephObject.setInterval(jsonObj.getString("interval"));
+		 * josephObject.setPersons(jsonObj.getJSONArray("persons"));
+		 * 
+		 * request.setCircle(josephObject);
+		 */
+
+		JosephCircleRequest request = new JosephCircleRequest();
+		JSONObject jsonObj = jsonObject.getJSONObject("circle");
+
+		JosephObjectConverter josephObjectConverter = new JosephObjectConverter();
+        
+		if (jsonObj != null) {
+			request.setCircle(josephObjectConverter.fromJson(jsonObj));
+			
+		} else {
+			request.setCircle(null);
+		}
+
+		return request;
+	}
+
+	@Override
+	public JSONObject toJson(JosephCircleRequest objJson) {
+
+		return null;
+	}
+
+}
